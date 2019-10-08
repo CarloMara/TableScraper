ca7bb017fb6c6d7f50cd72cd86d018248d0a96dd
Carlo <ste.maragno@gmail.com>
2019-10-08 16:13:55 +0200

    Split functions in heatbox, general refactor

--- heatmap.py
+++ heatmap.py
@@ -2,6 +2,9 @@ import numpy as np
 import cv2
 import projection_sampling as sample
 import util
+import heat_box as hb
+import text_box as box
+from IPython import embed
 
 
 def heat_map(img, height_threshold, draw_dots=False, draw_boxes=False, draw_chunks=False):
@@ -48,20 +51,62 @@ def draw_dots(cimg, img, height_threshold):
     (H, W) = img.shape[:2]
     edges = cv2.Canny(cimg, 275, 500, apertureSize=3)
     y_projection = sample.horizontal_projection(edges, 0, W, 0, H)
-    util.draw_dots_for_list_horizontal(img, y_projection, H)
+    util.draw_dots_for_list_horizontal(img, y_projection, H, color=util.rgb_to_bgr((144, 89, 47)))
     # find vertical chunks
     y_chunks = sample.hysteresis_thresholding(y_projection)
     for y_tuple in y_chunks:
         if y_tuple[1]-y_tuple[0] > height_threshold:
             x_projection = sample.vertical_projection(edges, y_tuple[0], y_tuple[1])
-            util.draw_dots_from_list(img, x_projection, y_tuple[1] - y_tuple[0], y_tuple[0], (155,155,155))
+            util.draw_dots_from_list(img, x_projection, y_tuple[1] - y_tuple[0], y_tuple[0],
+                                     color=util.rgb_to_bgr((150, 139, 116)))
 
 
-def draw_chunks(cimg, img, height_threshold):
+def draw_chunks_line(cimg, img, height_threshold):
     (H, W) = img.shape[:2]
     edges = cv2.Canny(cimg, 275, 500, apertureSize=3)
     y_projection = sample.horizontal_projection(edges, 0, W, 0, H)
     y_chunks = sample.hysteresis_thresholding(y_projection)
     for y_tuple in y_chunks:
         if y_tuple[1] - y_tuple[0] > height_threshold:
-         util.draw_chunk_line(img, y_tuple, W)
+            util.draw_chunk_line(img, y_tuple, W)
+
+
+def draw_chunks_box(cimg, img, height_threshold, width_threshold):
+    (H, W) = img.shape[:2]
+    edges = cv2.Canny(cimg, 275, 500, apertureSize=3)
+    y_projection = sample.horizontal_projection(edges, 0, W, 0, H)
+    y_chunks = sample.hysteresis_thresholding(y_projection)
+    for y_tuple in y_chunks:
+        if y_tuple[1]-y_tuple[0] > height_threshold:
+            x_projection = sample.vertical_projection(edges, y_tuple[0], y_tuple[1])
+            x_chunks = sample.hysteresis_thresholding(x_projection)
+            for x_tuple in x_chunks:
+                if x_tuple[1] - x_tuple[0] > width_threshold:
+                    util.draw_chunk_box(img, x_tuple, y_tuple, color=util.rgb_to_bgr((255,181,22)))
+
+
+def boxes_from_img(img, height_threshold, width_threshold):
+    (H, W) = img.shape[:2]
+    edges = cv2.Canny(img, 275, 500, apertureSize=3)
+
+    y_projection = sample.horizontal_projection(edges, 0, W, 0, H)
+
+    box_coordinates = []
+
+    # find vertical chunks
+    y_chunks = sample.hysteresis_thresholding(y_projection)
+
+    for y_tuple in y_chunks:
+
+        # analyze only big chunks
+        if y_tuple[1]-y_tuple[0] > height_threshold:
+            x_projection = sample.vertical_projection(edges, y_tuple[0], y_tuple[1])
+
+            # find horizontal chunks
+            x_chunks = sample.hysteresis_thresholding(x_projection)
+
+            for x_tuple in x_chunks:
+                if x_tuple[1] - x_tuple[0] > width_threshold:
+                    box_coordinates.append(hb.HeatBox(x_tuple[0], x_tuple[1], y_tuple[0], y_tuple[1]))
+
+    return box_coordinates
