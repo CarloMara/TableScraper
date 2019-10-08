d146d0ccc61e43378a22131003fc0876f372e3d9
Carlo <ste.maragno@gmail.com>
2019-10-08 23:01:24 +0200

    Update demo

--- demo.py
+++ demo.py
@@ -24,27 +24,24 @@ if __name__ == "__main__":
     # hm.draw_dots(img, work_img, 20)
     # hm.draw_chunks_line(img, work_img, 20)
     # hm.draw_chunks_box(img, work_img, 20, 5)
-
     mine = pe.PdfExtractor(w=W, h=H, file='../isa_m16c60.pdf')
-
     text_boxes = mine.get_text_chunks(166)
-
     visual_elements = hm.boxes_from_img(img, 20, 5)
-
     aggregator = aggr.Aggregator(visual_elements, text_boxes)
-
     aggregator.draw_stuff(work_img)
 
-    for k in aggregator.page_elements:
-        print("IMG Chunk: ")
-        for elm in k.sorted_children:
-            print(elm)
-        print("\n")
+    instructions = aggregator.get_instructions()
+
+
+    # for k in aggregator.page_elements:
+    #     print("IMG Chunk: ")
+    #     for elm in k.sorted_children:
+    #         print(elm)
+    #     print("\n")
 
     for text in text_boxes:
         util.draw_chunk_box(img, (int(text.x0), int(text.x1)),
                             (int(text.y0), int(text.y1)), color=util.rgb_to_bgr((255, 181, 22)))
-
 
     view = sdl.SdlView((W, H))
 
