b04e3562432b209a1b67ad2c0a1bd7c9f1ce6148
Carlo <ste.maragno@gmail.com>
2019-10-09 17:02:07 +0200

    Add mnemonic detection

--- instruction_box.py
+++ instruction_box.py
@@ -1,11 +1,12 @@
 class InstructionBox:
 
-    def __init__(self, y0, y1, title, elements):
+    def __init__(self, y0, y1, title, elements, mnemonics):
         self.y0 = y0
         self.y1 = y1
         self.yc = (y1 + y0)/2
         self.title = title
         self.elements = self.add_elements(elements)
+        self.mnemonic = self.add_mnemonic(mnemonics)
 
     def add_elements(self, elements):
         # checks if element is inside instruction and adds it to the elements list
@@ -16,10 +17,14 @@ class InstructionBox:
 
         return instruction_elements
 
-    @staticmethod
-    def add_mnemonic(elements):
+    def add_mnemonic(self, mnemonics):
         # checks if a mnemonic is inside instruction and adds it to the mnemonics list
-        return NotImplemented
+        instruction_mnemonics = []
+        for mnemonic in mnemonics:
+            if self.is_container(mnemonic):
+                instruction_mnemonics.append(mnemonic)
+
+        return instruction_mnemonics
 
     def is_container(self, chunk, y_tolerance=2):
         if (self.y0 - y_tolerance) <= chunk.yc <= (self.y1 + y_tolerance) and \
