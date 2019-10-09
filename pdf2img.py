cd196442e21fc8708146eea9f3493a499382585b
Carlo <ste.maragno@gmail.com>
2019-10-09 17:05:16 +0200

    Add pdf2img class
    This is a dumb stub that passes the requested page to convert via an os call

---
+++ pdf2img.py
@@ -0,0 +1,20 @@
+import os
+import cv2
+
+class Pdf2Img:
+
+    def __init__(self, path, W, H):
+        self.path = path
+        self.W = W
+        self.H = H
+
+    def get_page(self, pageno):
+        # print(pageno)
+        cmd = "convert -density 150 -background white -alpha remove \"" + self.path + "[" + str(pageno-1) + "]" + \
+              "\" -quality 90 -colorspace RGB page.png"
+        os.system(cmd)
+
+        img = cv2.imread('./page.png')
+        img = cv2.resize(img, (self.W, self.H))
+
+        return img
